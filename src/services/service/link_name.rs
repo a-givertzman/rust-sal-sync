74f47f193aef4edd0c7236b0fc8079dae41b0d21
a-givertzman <a.givertzman@icloud.com>
2024-09-11 19:46:54 +0300

    Services | LinkName |  + Send + Sync

--- src/services/service/link_name.rs
+++ src/services/service/link_name.rs
@@ -1,23 +1,26 @@
-use std::cell::RefCell;
+use std::str::FromStr;
+use concat_in_place::strcat;
 ///
 /// Contains Name of the Link (Channel / Queue) in the separate format
 /// Service.Link -> Service & Link
 #[derive(Debug, Clone, PartialEq)]
 pub struct LinkName {
     name: String,
-    service: RefCell<Option<String>>,
-    link: RefCell<Option<String>>,
+    service: String,
+    link: String,
 }
 ///
 /// Contains the Service's Link name in the format 'Servece.Link'
 impl LinkName {
     ///
     /// Creates new instance of the LinkName from the string like "Service.Link"
-    pub fn new(name: impl Into<String>) -> Self {
+    pub fn new(service: impl Into<String>, link: impl Into<String>) -> Self {
+        let service = service.into();
+        let link = link.into();
         Self {
-            name: name.into(),
-            service: RefCell::new(None),
-            link: RefCell::new(None),
+            name: strcat!(service.as_str() "." link.as_str()),
+            service,
+            link,
         }
     }
     ///
@@ -26,78 +29,19 @@ impl LinkName {
         self.name.clone()
     }
     ///
-    /// Splits name 'Service.Link' into 
-    /// - Service
-    /// - Link
-    fn split_(&self) -> Result<(), String> {
-        let parts: Vec<&str> = self.name.split('.').collect();
-        match parts.first() {
-            Some(value) => {
-                self.service.borrow_mut().replace(value.to_owned().to_owned());
-                match parts.get(1) {
-                    Some(value) => {
-                        self.link.borrow_mut().replace(value.to_owned().to_owned());
-                        Ok(())
-                    }
-                    None => Err(format!("LinkName.split_ | '{}' does not have structure 'Service.Link'", self.name)),
-                }
-            }
-            None => Err(format!("LinkName.split_ | '{}' does not have structure 'Service.Link'", self.name)),
-        }
-    }
-    ///
-    /// Returns splitted Service and Link names
-    pub fn split(&self) -> Result<(String, String), String> {
-        if self.service.borrow().is_none() || self.link.borrow().is_none() {
-            if let Err(err) = self.split_() {
-                return Err(err);
-            };
-        }
-        match self.service.borrow().clone() {
-            Some(service) => {
-                match self.link.borrow().clone() {
-                    Some(link) => {
-                        Ok((service, link))
-                    }
-                    None => Err(format!("LinkName.split | Part 'Link' not found in the '{}'", self.name)),
-                }
-            }
-            None => Err(format!("LinkName.split | Part 'service' not found in the '{}'", self.name)),
-        }
+    /// Returns splitted Service and Link
+    pub fn split(&self) -> (String, String) {
+        (self.service.clone(), self.link.clone())
     }
     ///
     /// Returns the Service name
-    pub fn service(&self) -> Result<String, String> {
-        if self.service.borrow().is_none() {
-            if let Err(err) = self.split_() {
-                return Err(err);
-            };
-        }
-        match self.service.borrow().clone() {
-            Some(service) => Ok(service),
-            None => Err(format!("LinkName.service | Part 'service' not found in the '{}'", self.name)),
-        }
+    pub fn service(&self) -> String {
+        self.service.clone()
     }
     ///
     /// Returns the Service's Link name
-    pub fn link(&self) -> Result<String, String> {
-        if self.link.borrow().is_none() {
-            if let Err(err) = self.split_() {
-                return Err(err);
-            };
-        }
-        match self.link.borrow().clone() {
-            Some(link) => Ok(link),
-            None => Err(format!("LinkName.link | Part 'Link' not found in the '{}'", self.name)),
-        }
-    }
-    ///
-    /// Try to split, panics if split() returns err
-    pub fn validate(self) -> Self {
-        if let Err(err) = self.split() {
-            panic!("LinkName.validate | Error: '{}'", err)
-        }
-        self
+    pub fn link(&self) -> String {
+        self.link.clone()
     }
 }
 //
@@ -105,5 +49,29 @@ impl LinkName {
 impl std::fmt::Display for LinkName {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.name)
+    }
+}
+//
+//
+impl FromStr for LinkName {
+    type Err = String;
+    ///
+    /// Creates new instance of the LinkName from the string like "Service.Link"  
+    /// Spliting name 'Service.Link' into 
+    /// - Service
+    /// - Link
+    fn from_str(name: &str) -> Result<Self, Self::Err> {
+        let parts: Vec<&str> = name.split('.').collect();
+        match parts.first() {
+            Some(service) => {
+                match parts.get(1) {
+                    Some(link) => {
+                        Ok(Self { name: name.to_owned(), service: service.to_string(), link: link.to_string() })
+                    }
+                    None => Err(format!("LinkName.from_str | '{}' does not have structure 'Service.Link'", name)),
+                }
+            }
+            None => Err(format!("LinkName.from_str | '{}' does not have structure 'Service.Link'", name)),
+        }
     }
 }
