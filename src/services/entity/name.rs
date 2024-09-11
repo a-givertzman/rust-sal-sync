5ffba64f327094ffa29e070e06fba141078fe7e3
a-givertzman <a.givertzman@icloud.com>
2024-09-11 20:14:36 +0300

    Services | Name |  + Send + Sync + test

--- src/services/entity/name.rs
+++ src/services/entity/name.rs
@@ -1,6 +1,5 @@
-use std::{cell::RefCell, fmt::{Debug, Display}};
+use std::fmt::{Debug, Display};
 use concat_string::concat_string;
-use log::error;
 ///
 /// Creates Point name from parts, dividing parts with single "/" character
 /// - path1 + "" = /path1
@@ -12,7 +11,7 @@ use log::error;
 pub struct Name {
     parent: String,
     me: String,
-    joined: RefCell<Option<String>>,
+    joined: String,
 }
 //
 // 
@@ -20,67 +19,73 @@ impl Name {
     ///
     /// Creates new instance of Name from 'parent' and 'me'
     pub fn new(parent: impl Into<String>, me: impl Into<String>) -> Self {
+        let parent = parent.into();
+        let me = me.into();
+        let joined = Self::joined_(&parent, &me);
         Self {
-            parent: parent.into(),
-            me: me.into(),
-            joined: RefCell::new(None),
+            parent,
+            me,
+            joined,
         }
     }
     ///
-    /// Returns 
-    pub fn join(&self) -> String {
-        if self.joined.borrow().is_none() {
-            let parent = match self.parent.chars().next() {
-                Some(parent_first) => {
-                    if parent_first == '/' {
-                        self.parent.clone()
-                    } else {
-                        concat_string!("/", self.parent)
-                    }
+    /// Returns joined as '/parent/me
+    fn joined_(parent: &str, me: &str) -> String {
+        let parent = match parent.chars().next() {
+            Some(parent_first) => {
+                if parent_first == '/' {
+                    parent.to_owned()
+                } else {
+                    concat_string!("/", parent)
                 }
-                None => {
-                    "/".to_owned()
-                }
-            };
-            *self.joined.borrow_mut() = Some(match parent.chars().last() {
-                Some(parent_last) => {
-                    match self.me.chars().next() {
-                        Some(me_first) => {
-                            if parent_last == '/' && me_first == '/' {
-                                concat_string!(parent, self.me[1..])
-                            } else if parent_last == '/' && me_first != '/' {
-                                concat_string!(parent, self.me)
-                            } else if parent_last != '/' && me_first == '/' {
-                                concat_string!(parent, self.me)
-                            } else {
-                                concat_string!(parent, "/", self.me)
-                            }
-                        }
-                        None => {
-                            parent.to_owned()
+            }
+            None => {
+                "/".to_owned()
+            }
+        };
+        match parent.chars().last() {
+            Some(parent_last) => {
+                match me.chars().next() {
+                    Some(me_first) => {
+                        if parent_last == '/' && me_first == '/' {
+                            concat_string!(parent, me[1..])
+                        } else if parent_last == '/' && me_first != '/' {
+                            concat_string!(parent, me)
+                        } else if parent_last != '/' && me_first == '/' {
+                            concat_string!(parent, me)
+                        } else {
+                            concat_string!(parent, "/", me)
                         }
                     }
-                }
-                None => {
-                    match self.me.chars().next() {
-                        Some(me_first) => {
-                            if me_first == '/' {
-                                self.me.clone()
-                            } else {
-                                concat_string!("/", self.me)
-                            }
-                        }
-                        None => {
-                            panic!("PointName.new | Parent or name must not be empty")
-                        }
+                    None => {
+                        parent.to_owned()
                     }
                 }
-            });
+            }
+            None => {
+                match me.chars().next() {
+                    Some(me_first) => {
+                        if me_first == '/' {
+                            me.to_owned()
+                        } else {
+                            concat_string!("/", me)
+                        }
+                    }
+                    None => {
+                        panic!("PointName.new | Parent or name can't be empty")
+                    }
+                }
+            }
         }
-        self.joined.borrow().clone().unwrap_or_else(|| {
-            error!("PointName.new | Join error for parent: '{}', me: '{}'", self.parent, self.me);
-            String::new()
-        })
+        // joined.unwrap_or_else(|| {
+        //     error!("PointName.new | Join error for parent: '{}', me: '{}'", self.parent, me);
+        //     String::new()
+        // })
+    }
+    ///
+    /// Returns joined as '/parent/me'
+    pub fn join(&self) -> String {
+        self.joined.clone()
     }
     ///
     /// Returns original parent
@@ -131,15 +136,15 @@ impl From<Name> for String {
 //
 // 
 impl From<String> for Name {
-    fn from(value: String) -> Self {
-        Name { parent: value, me: String::new(), joined: RefCell::new(None) }
+    fn from(parent: String) -> Self {
+        Name::new(parent, "")
     }
 }
 //
 // 
 impl From<&str> for Name {
-    fn from(value: &str) -> Self {
-        Name { parent: value.to_owned(), me: String::new(), joined: RefCell::new(None) }
+    fn from(parent: &str) -> Self {
+        Name::new(parent, "")
     }
 }
 ///
