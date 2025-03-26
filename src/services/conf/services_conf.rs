b5398fcaa05037433c411841123747ae65a01246
Anton Lobanov <a.givertzman@icloud.com>
2025-03-26 11:50:21 +0300

    Services | ServicesConf

--- src/services/conf/services_conf.rs
+++ src/services/conf/services_conf.rs
@@ -1,6 +1,7 @@
 use crate::services::retain::retain_conf::RetainConf;
 ///
 /// Configuration parameters for [Services](src/serv)
+#[derive(Debug, PartialEq, Clone)]
 pub struct ServicesConf {
     pub retain: RetainConf,
 }
