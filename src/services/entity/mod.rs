17f7ca5c9d0da10cf1418d59423165a7c22116bb
Anton Lobanov <a.givertzman@icloud.com>
2025-03-31 17:49:12 +0300

    Cargo.toml | updated sal-core = 0.0.3

--- src/services/entity/mod.rs
+++ src/services/entity/mod.rs
@@ -2,6 +2,7 @@
 //! # Common application entities
 //! 
 pub mod cot;
+#[deprecated(note = "Use sal_core::dbg::Dbg from https://github.com/a-givertzman/rust-sal-core")]
 pub mod dbg_id;
 pub mod name;
 pub mod object;
