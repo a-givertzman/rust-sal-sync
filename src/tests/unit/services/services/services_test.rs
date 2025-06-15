42403c7e19664461ffaa726d0c75f68ea324b148
Anton Lobanov <a.givertzman@icloud.com>
2025-06-15 17:55:35 +0300

    Services | API | Services test

--- src/tests/unit/services/services/services_test.rs
+++ src/tests/unit/services/services/services_test.rs
@@ -1,12 +1,12 @@
 #[cfg(test)]
 
 mod services {
-    use std::{env, sync::{atomic::{AtomicBool, Ordering}, Arc, Once}, time::Duration};
+    use std::{env, sync::{atomic::{AtomicBool, Ordering}, Arc, Once}, time::{Duration, Instant}};
     use coco::Stack;
     use sal_core::dbg::Dbg;
     use testing::stuff::{max_test_duration::TestDuration};
     use debugging::session::debug_session::{DebugSession, LogLevel, Backtrace};
-    use crate::{services::{conf::{ConfTree, ServicesConf}, entity::{Name, Object}, Service, Services}, sync::WaitBox, thread_pool::Scheduler};
+    use crate::{services::{conf::{ConfTree, ServicesConf}, entity::{Name, Object}, Service, Services}, sync::WaitBox, thread_pool::{Scheduler, ThreadPool}};
     ///
     ///
     static INIT: Once = Once::new();
@@ -34,6 +34,7 @@ mod services {
         let test_duration = TestDuration::new(&dbg, Duration::from_secs(10));
         test_duration.run().unwrap();
         log::trace!("dir: {:?}", env::current_dir());
+        let time = Instant::now();
         let services = Arc::new(Services::new(&dbg, ServicesConf::new(
             &dbg, 
             ConfTree::empty(),
@@ -46,16 +47,6 @@ mod services {
             services.insert(task);
             name
         }).collect();
-        // let target  = 3;
-        // let points = services.points(dbg).then(|points| points, |err| {
-        //     log::error!("{}.handle.Subscribe | Requesting points error: {:?}", dbg, err);
-        //     vec![]
-        // });
-        // let points_count = points.len();
-        // log::debug!(" points count: {:?}", points_count);
-        // for point in points {
-        //     log::debug!("\t {:?}", point);
-        // }
         // assert!(points_count == target, "\nresult: {:?}\ntarget: {:?}", points_count, target);
         for t in tasks {
             let task = services.get(&t.join()).unwrap();
@@ -64,6 +55,45 @@ mod services {
         }
         services.exit();
         services.wait().unwrap();
+        log::info!("{dbg} | All finished in {:?}", time.elapsed());
+        test_duration.exit();
+    }
+    ///
+    /// Testing `Services` on `std::thread`
+    #[test]
+    fn services_scheduler() {
+        DebugSession::init(LogLevel::Info, Backtrace::Short);
+        init_once();
+        init_each();
+        let dbg = Dbg::own("test-Services-thread");
+        log::debug!("\n{}", dbg);
+        let tasks = 100;
+        let test_duration = TestDuration::new(&dbg, Duration::from_secs(10));
+        test_duration.run().unwrap();
+        log::trace!("dir: {:?}", env::current_dir());
+        let time = Instant::now();
+        let thread_pool = ThreadPool::new(&dbg, None);
+        let services = Arc::new(Services::new(&dbg, ServicesConf::new(
+            &dbg, 
+            ConfTree::empty(),
+        ), Some(thread_pool.scheduler())));
+        services.run().unwrap();
+        let tasks: Vec<Name> = (0..tasks).map(|i| {
+            let task = Arc::new(ServiceMok::new(&dbg, i, Some(thread_pool.scheduler())));
+            let name = task.name();
+            task.run().unwrap();
+            services.insert(task);
+            name
+        }).collect();
+        // assert!(points_count == target, "\nresult: {:?}\ntarget: {:?}", points_count, target);
+        for t in tasks {
+            let task = services.get(&t.join()).unwrap();
+            task.exit();
+            task.wait().unwrap();
+        }
+        services.exit();
+        services.wait().unwrap();
+        log::info!("{dbg} | All finished in {:?}", time.elapsed());
         test_duration.exit();
     }
     ///
@@ -90,6 +120,14 @@ mod services {
                 exit: Arc::new(AtomicBool::new(false)),
             }
         }
+        fn run_(dbg: Dbg, exit: Arc<AtomicBool>) {
+            loop {
+                std::thread::sleep(Duration::from_millis(50));
+                if exit.load(Ordering::SeqCst) {
+                    break;
+                }
+            }
+        }
     }
     impl Object for ServiceMok {
         fn name(&self) -> Name {
@@ -98,23 +136,31 @@ mod services {
     }
     impl Service for ServiceMok {
         fn run(&self) -> Result<(), sal_core::error::Error> {
+            let dbg = self.dbg.clone();
             let exit = self.exit.clone();
-            let h = self.schrduler.as_ref().unwrap().spawn(move|| {
-                loop {
-                    std::thread::sleep(Duration::from_millis(50));
-                    if exit.load(Ordering::SeqCst) {
-                        break;
-                    }
+            let h: Box<dyn WaitBox<()>> = match &self.schrduler {
+                Some(schrduler) => {
+                    let h = schrduler.spawn(move|| {
+                        Self::run_(dbg, exit);
+                        Ok(())
+                    })?;
+                    Box::new(h)
                 }
-                Ok(())
-            })?;
-            self.handle.push(Box::new(h));
+                None => {
+                    let h = std::thread::spawn(move|| {
+                        Self::run_(dbg, exit);
+                    });
+                    Box::new(h)
+                }
+            };
+            self.handle.push(h);
             Ok(())
         }
-    
+        //
         fn is_finished(&self) -> bool {
             self.is_finished.load(Ordering::SeqCst)
         }
+        //
         fn wait(&self) -> Result<(), sal_core::error::Error> {
             while !self.handle.is_empty() {
                 if let Some(handle) = self.handle.pop() {
@@ -125,6 +171,7 @@ mod services {
             }
             Ok(())
         }
+        //
         fn exit(&self) {
             self.exit.store(true, Ordering::SeqCst);
         }
