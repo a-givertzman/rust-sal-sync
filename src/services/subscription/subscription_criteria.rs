6c9a33be5a0becd4de692e612809fa86577d946a
Anton Lobanov <a.givertzman@icloud.com>
2026-02-01 05:31:54 +0300

    SubscriptionCriteria | Fixes

--- src/services/subscription/subscription_criteria.rs
+++ src/services/subscription/subscription_criteria.rs
@@ -2,8 +2,11 @@ use concat_string::concat_string;
 use crate::services::entity::Cot;
 ///
 /// Detailed definition of the subscription;
-/// - "name" - the name of the point to be subscribed;
-/// - "cot" - the cause & direction of the transmission to be subscribed;
+/// - `name` - the name of the point to be subscribed;
+/// - `cot` - the cause & direction of the transmission to be subscribed;
+/// - `dest` - The destionation string of the Subscription in the format `Cot:point name`
+///     - Winch.PointName           - Cot::All
+///     - Inf:Winch.PointName       - Cot::Inf only
 #[derive(Debug, Clone, PartialEq)]
 pub struct SubscriptionCriteria {
     name: String,
@@ -15,8 +18,8 @@ pub struct SubscriptionCriteria {
 impl SubscriptionCriteria {
     ///
     /// Detailed definition of the subscription;
-    /// - "name" - full name of the point to be subscribed;
-    /// - "cot" - the cause & direction of the transmission to be subscribed;
+    /// - `name` - full name of the point to be subscribed;
+    /// - `cot` - the cause & direction of the transmission to be subscribed;
     pub fn new(name: impl Into<String>, cot: Cot) -> Self {
         let name = name.into();
         Self {
@@ -26,12 +29,12 @@ impl SubscriptionCriteria {
         }
     }
     ///
-    /// The destionation string of the Subscription in the format "Cot:point name"
+    /// The destionation string of the Subscription in the format `Cot:point name`
     pub fn destination(&self) -> String {
         self.dest.clone()
     }
     ///
-    /// The destionation string of the Subscription in the format "Cot:point name"
+    /// The destionation string of the Subscription in the format `Cot:point name`
     pub fn dest(cot: &Cot, name: &str) -> String {
         match cot {
             Cot::All => name.to_owned(),
